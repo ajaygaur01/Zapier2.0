2ea9b7f9d22900c316170d1d443cc3408e6bf554
Ajay Gaur <gaurajay787@gmail.com>
2026-04-14 23:47:15 +0530

    craeted hooks for catching hooksurl

---
+++ apps/hooks/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { Prisma, PrismaClient } from "@repo/db";
+const app = express();
+app.use(express.json());
+const prisma = new PrismaClient();
+
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const { userId, zapId } = req.params;
+  const body = req.body;
+
+  try {
+    await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+      const zapRun = await tx.zapRun.create({
+        data: {
+          metadata: body,
+          zapId,
+        },
+      });
+
+      await tx.zapRunOutbox.create({
+        data: {
+          zapRunId: zapRun.id,
+        },
+      });
+    });
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error, "error in hooks");
+    res.status(500).json({ success: false });
+  }
+
+});
+
+app.listen(8000, () => {
+  console.log("hooks port running on 8000");
+});
