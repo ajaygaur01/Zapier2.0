e63048ec5580141b9864a600dd6903c29933588f
Ajay Gaur <gaurajay787@gmail.com>
2026-06-13 09:28:52 +0530

    migrate frontend to argo rollouts canary deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -611,3 +611,13 @@ resource "helm_release" "argocd" {
     value = "512Mi"
   }
 }
+
+# Install Argo Rollouts Controller
+resource "helm_release" "argo_rollouts" {
+  name             = "argo-rollouts"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-rollouts"
+  namespace        = "argo-rollouts"
+  create_namespace = true
+}
+
